8f9637e0ba7981a1bf553fc9f73d447c4bde8213
Noxeal <mael.lrouzic@gmail.com>
2025-04-18 10:06:14 +0200

    Div de maintenance dans bicycle card

--- app/src/components/commons/BicycleCard.vue
+++ app/src/components/commons/BicycleCard.vue
@@ -1,18 +1,25 @@
 <template>
     <div :class="['bicycle-infos', { selected: selected }]">
-      <div class="photo">
-        <img :src="photoUrl" alt="Photo du vélo" v-if="bicycle.photo" />
-      </div>
-  
-      <div class="details">
-        <h2>{{ bicycle.nom }}</h2>
-        <p v-if="!is_list_element">{{ bicycle.description }}</p>
-        <p><strong>État :</strong> {{ bicycle.etat }}</p>
-        <p v-if="!is_list_element"><strong>Type :</strong> {{ bicycle.type }}</p>
-        <p><strong>Année de mise en service :</strong> {{ bicycle.annee_mise_en_service }}</p>
-        <button v-if="can_modify" class="update-button" @click.stop="showUpdateModal = true">Modifier</button>
-      </div>
-  
+        <div class="bicycle-card-container">
+            <div class="bicycle-basic-card">
+                <div class="photo">
+                    <img :src="photoUrl" alt="Photo du vélo" v-if="bicycle.photo" />
+                </div>
+                <div class="details">
+                    <h2>{{ bicycle.nom }}</h2>
+                    <p v-if="!is_list_element">{{ bicycle.description }}</p>
+                    <p><strong>État :</strong> {{ bicycle.etat }}</p>
+                    <p v-if="!is_list_element"><strong>Type :</strong> {{ bicycle.type }}</p>
+                    <p><strong>Année de mise en service :</strong> {{ bicycle.annee_mise_en_service }}</p>
+                    <button v-if="can_modify" class="update-button" @click.stop="showUpdateModal = true">Modifier</button>
+                </div>
+            </div>
+
+            <div class="maintenance-details" v-if="!is_list_element">
+                <p v-if="bicycle.maintenance"><strong>Maintenance :</strong> {{ bicycle.etat_maintenance }}</p>
+                <p v-if="bicycle.maintenance"><strong>État de maintenance :</strong> {{ bicycle.etat_maintenance }}</p>
+            </div>
+        </div>
       <!-- Modale de mise à jour -->
       <Modal v-if="showUpdateModal" @close="showUpdateModal = false">
         <h2>Modifier le vélo</h2>
@@ -112,6 +119,12 @@
     border-radius: 8px;
 }
 
+/* .bicycle-infos:hover {
+    transform: scale(1.05);
+    transition: transform 0.2s;
+    cursor: pointer;
+} */
+
 .bicycle-infos.selected {
     background-color: var(--color-soft-blue);
     outline: 5px solid var(--color-pink);
@@ -129,6 +142,7 @@
 
 .details h2 {
     margin-bottom: 10px;
+    font-size: 1.2rem;
 }
 
 .details p {
@@ -171,4 +185,17 @@
     border-radius: 5px;
     border: 1px solid #ccc;
 }
+
+.bicycle-card-container {
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+}
+
+
+.bicycle-basic-card {
+    display: flex;
+    gap: 20px;
+    background-color: var(--color-dark-blue);
+    color: white;}
 </style>
